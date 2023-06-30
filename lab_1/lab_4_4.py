1582bdd3cb9e777f3bf6c9fa39894d61a87b2015
churilos <138076685+churilos@users.noreply.github.com>
2023-06-30 22:10:42 +0900

    Update lab_4_4.py

--- lab_1/lab_4_4.py
+++ lab_1/lab_4_4.py
@@ -1,61 +1,57 @@
-class Encoder:
-    def encode(self, string):
-        pass
+import math
 
-    def decode(self, string):
-        pass
+class HammingEncoder:
+    def __init__(self, dataBits):
+        self.dataBits = dataBits
+        self.controlBits = 0
+        while (2 ** self.controlBits) <= (self.controlBits + self.dataBits):
+            self.controlBits += 1
 
-class HuffmanEncoder(Encoder):
-    def __init__(self):
-        self.compressionCoef = 0
+    def encode(self, data):
+        data = list(data)
+        c = self.controlBits
+        n = len(data) + c
+        res = [''] * (n + 1)
+        j = 0
+        for i in range(1, n + 1):
+            if i == (2 ** j):
+                res[i] = '0'
+                j += 1
+            else:
+                res[i] = data[-1]
+                data.pop()
+        for i in range(c):
+            val = 0
+            for j in range(1, n + 1):
+                if j & (2 ** i) == (2 ** i):
+                    val = val ^ int(res[-1 * j])
+            res[(2 ** i)] = str(val)
+        return ''.join(res[::-1])
 
-    def encode(self, string):
-        # Implement the Huffman encoding algorithm here
-        # ...
-        self.__setCompressionCoef()
+    def decode(self, data):
+        n = len(data)
+        c = self.controlBits
+        res = [''] * (n + 1)
+        for i in range(n):
+            res[i + 1] = data[i]
+        b = [0] * c
+        for i in range(c):
+            val = 0
+            for j in range(1, n + 1):
+                if j & (2 ** i) == (2 ** i):
+                    val = val ^ int(res[-1 * j])
+            b[i] = val
+        err = sum(b[i] * (2 ** i) for i in range(c))
+        if err != 0:
+            res[err] = '0' if res[err] == '1' else '1'
+        decoded_data = ''
+        for i in range(1, n + 1):
+            if not math.log(i, 2).is_integer():
+                decoded_data += res[i]
+        return decoded_data
 
-    def decode(self, string):
-        # Implement the Huffman decoding algorithm here
-        # ...
 
-    def __setCompressionCoef(self):
-        # Calculate the compression coefficient for the Huffman algorithm here
-        # ...
-        pass
-
-    def getCompressionCoef(self):
-        return self.compressionCoef
-
-class LZEncoder(Encoder):
-    def __init__(self):
-        self.compressionCoef = 0
-
-    def encode(self, string):
-        # Implement the Lempel-Ziv encoding algorithm here
-        # ...
-        self.__setCompressionCoef()
-
-    def decode(self, string):
-        # Implement the Lempel-Ziv decoding algorithm here
-        # ...
-
-    def __setCompressionCoef(self):
-        # Calculate the compression coefficient for the Lempel-Ziv algorithm here
-        # ...
-        pass
-
-    def getCompressionCoef(self):
-        return self.compressionCoef
-
-string = "Python is a widely used high-level programming language for general-purpose programming, created by Guido van Rossum and first released in 1991."
-
-huffman_encoder = HuffmanEncoder()
-encoded_string = huffman_encoder.encode(string)
-decoded_string = huffman_encoder.decode(encoded_string)
-compression_coef = huffman_encoder.getCompressionCoef()
-
-lz_encoder = LZEncoder()
-encoded_string = lz_encoder.encode(string)
-decoded_string = lz_encoder.decode(encoded_string)
-compression_coef = lz_encoder.getCompressionCoef()
-
+hamming_encoder = HammingEncoder(4)
+encoded_string = hamming_encoder.encode("0101")
+decoded_string = hamming_encoder.decode(encoded_string)
+print(4, '\n', hamming_encoder, '\n', encoded_string, '\n', decoded_string)
