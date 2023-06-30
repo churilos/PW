5ee7e0b85a742962f09317513395ff092507cf95
churilos <138076685+churilos@users.noreply.github.com>
2023-06-30 18:33:16 +0900

    Update views.py

--- lab_3/PraktikumDjangoLabs/personal_cabinet/views.py
+++ lab_3/PraktikumDjangoLabs/personal_cabinet/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def index(request): 
   return render(request, 'personal_cabinet/index.html', {'var': 'Some values of its variable.'})
+
+def add(request):
+  return render(request, 'personal_cabinet/add.html', {'var': 'It\'s a page of adding data.'})
+
+  
+def add(request):
+  return render(request, 'personal_cabinet/view.html', {'var': 'It\'s a page of viewung data.'})
