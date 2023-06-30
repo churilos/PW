d551ed522934d8d22a82efe242d7acaf2bd18e11
churilos <138076685+churilos@users.noreply.github.com>
2023-06-30 18:12:58 +0900

    Update settings.py

--- lab_3/PraktikumDjangoLabs/PraktikumDjangoLabs/settings.py
+++ lab_3/PraktikumDjangoLabs/PraktikumDjangoLabs/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'PraktikumDjangoLabs'
+    'personal_cabinet'
 ]
 
 MIDDLEWARE = [
