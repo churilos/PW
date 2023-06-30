d0c63498d77d6d6fa6477ad28a527d9f32886775
churilos <138076685+churilos@users.noreply.github.com>
2023-06-30 21:20:25 +0900

    Update lab_3_9.py

--- lab_1/lab_3_9.py
+++ lab_1/lab_3_9.py
@@ -1,25 +1,23 @@
-'''
- Работа с файлами
-'''
-file1 = open("file1.txt", "r") # открыть на чтение
+import os
+file1 = open(f'{os.getcwd()}/experements/lab_3/file1.txt', "r") # открыть на чтение
 st = file1.read(1) # считать 1 символ
 st += file1.read() # считать до конца файла
 print("File1: ", st)
 file1.close()
 # считать построчно
-file2 = open("file2.txt", "r")
+file2 = open(f'{os.getcwd()}/experements/lab_3/file1.txt', "r")
 print("File2: ")
 i = 0
 for line in file2: # итерация по строкам файла
- print("Line {}: \
+  print("Line {}: \
 {}".format(i,line.replace("\n","")))
- i += 1
+  i += 1
 file2.close()
 # запись в файл
-file3 = open("file3.txt","w")
+file3 = open(f'{os.getcwd()}/experements/lab_3/file1.txt',"w")
 for s in "Strings can be easily written to file":
- file3.write(s)
- if s == " ":
- file3.write("\n")
+  file3.write(s)
+  if s == " ":
+    file3.write("\n")
 print("File 3 was written successfully")
 file3.close()
