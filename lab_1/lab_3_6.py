607568650ae3b9bc77a56bea9d2c4551ee9b1f70
churilos <138076685+churilos@users.noreply.github.com>
2023-06-30 20:44:44 +0900

    Update lab_3_6.py

--- lab_1/lab_3_6.py
+++ lab_1/lab_3_6.py
@@ -2,18 +2,18 @@
  Функции
 '''
 def dictUpdate(a):
- a.update([("x",5)])
- print("dict in function: ",a)
- return
+  a.update([("x",5)])
+  print("dict in function: ",a)
+  return
 def dictNoUpdate(a):
- a = a.copy()
- a.update([("y",3)])
- print("dict in function: ",a)
- return
+  a = a.copy()
+  a.update([("y",3)])
+  print("dict in function: ",a)
+  return
 def returnFunc(a):
- def f1(a):
- print("returned f1(a): ",a)
- return f1
+  def f1(a):
+    print("returned f1(a): ",a)
+  return f1
 d= {"v":7}
 dictUpdate(d)
 print("dict out of function: ",d)
@@ -25,11 +25,11 @@ f(2)
 print("\n")
 
 def returnMod():
-    def mod15(x):
-        result = x % 15
-        print(result)
-        return result
-    return mod15
+  def mod15(x):
+    result = x % 15
+    print(result)
+    return result
+  return mod15
 
 mod15 = returnMod()
 mod15(42)
